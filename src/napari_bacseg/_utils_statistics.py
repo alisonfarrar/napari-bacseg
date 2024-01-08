a6ae0217899b5a57539248a1ee49aaf5405bbd0c
Alison Farrar <springy179@gmail.com>
2024-01-08 16:24:07 +0000

    Added cell_max_brightness and cell_min_brightness to get_layer_statistics

--- src/napari_bacseg/_utils_statistics.py
+++ src/napari_bacseg/_utils_statistics.py
@@ -366,6 +366,8 @@ def get_layer_statistics(image, cell_mask, box, layer_names):
 
         try:
             cell_brightness = int(np.mean(cell_image_crop[cell_mask_crop != 0]))
+            cell_max_brightness = int(np.max(cell_image_crop[cell_mask_crop != 0]))
+            cell_min_brightness = int(np.min(cell_image_crop[cell_mask_crop != 0]))
             cell_background_brightness = int(np.mean(cell_image_crop[cell_mask_crop == 0]))
             cell_contrast = cell_brightness / cell_background_brightness
             cell_laplacian = int(cv2.Laplacian(cell_image_crop, cv2.CV_64F).var())
@@ -374,7 +376,11 @@ def get_layer_statistics(image, cell_mask, box, layer_names):
             cell_contrast = None
             cell_laplacian = None
 
-        stats = {"cell_brightness[" + layer + "]": cell_brightness, "cell_contrast[" + layer + "]": cell_contrast, "cell_laplacian[" + layer + "]": cell_laplacian, }
+        stats = {"cell_brightness[" + layer + "]": cell_brightness,
+                 "cell_contrast[" + layer + "]": cell_contrast,
+                 "cell_laplacian[" + layer + "]": cell_laplacian,
+                 "cell_max_brightness[" + layer + "]": cell_max_brightness,
+                 "cell_min_brightness[" + layer + "]": cell_min_brightness,}
 
         layer_statistics = {**layer_statistics, **stats}
 
